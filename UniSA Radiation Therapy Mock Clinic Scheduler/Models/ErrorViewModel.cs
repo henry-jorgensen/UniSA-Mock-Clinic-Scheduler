6e0c768905f847cbdcb69cb48666365d177d93b5
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-08-01 19:08:18 +0930

    Add project files.

---
+++ UniSA Radiation Therapy Mock Clinic Scheduler/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniSA_Radiation_Therapy_Mock_Clinic_Scheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
