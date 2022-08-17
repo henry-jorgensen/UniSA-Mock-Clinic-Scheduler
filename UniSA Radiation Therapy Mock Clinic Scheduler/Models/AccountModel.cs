af1bb575bbd8a7b0cbd160e4ced66161cda3f5c8
dan <fildy004@mymail.unisa.edu.au>
2022-08-17 21:49:51 +0930

    Missing files

---
+++ UniSA Radiation Therapy Mock Clinic Scheduler/Models/AccountModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniSA_Radiation_Therapy_Mock_Clinic_Scheduler.Models
+{
+    public class AccountModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string CCCode { get; set; }
+
+    }
+}
+
