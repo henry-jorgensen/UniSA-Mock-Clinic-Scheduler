99a8aef43d31f241046da7fb00851d1e36d80cca
dan <fildy004@mymail.unisa.edu.au>
2022-08-31 11:28:12 +0930

    Moved course coordinator to separate controller

--- UniSA Radiation Therapy Mock Clinic Scheduler/Controllers/HomeController.cs
+++ UniSA Radiation Therapy Mock Clinic Scheduler/Controllers/HomeController.cs
@@ -30,8 +30,8 @@ namespace UniSA_Radiation_Therapy_Mock_Clinic_Scheduler.Controllers
             {
                 if (firebase.LoggedInAsCoordinator(UserToken).Result == true)
                 {
-                    return RedirectToAction("Create", "Home");
-                }
+                    return RedirectToAction("Create", "Coordinator");
+                }   
                 else
                 {
                     return RedirectToAction("Clinics", "Home");
@@ -69,112 +69,6 @@ namespace UniSA_Radiation_Therapy_Mock_Clinic_Scheduler.Controllers
             {
                 return RedirectToAction("Login", "Account");
             }
-        }
-
-        //Create clinics page
-        //Must be logged into course coordinator account to see
-        public IActionResult Create()
-        {
-            var _UserToken = HttpContext.Session.GetString("_UserToken");
-
-            if (firebase.LoggedInAsCoordinator(_UserToken).Result == true)
-            {
-                ViewBag.CurrentUser = firebase.GetUserModelAsync(_UserToken).Result;
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Login", "Account");
-            }
-        }
-
-        [HttpPost]
-        public IActionResult DoSomethingWithFirebase(string value)
-        {
-            string response = value + " Success";
-            return Ok(response);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateAClass(string name, string studyPeriod, string semester, string year)
-        {
-            ClassModel classModel = new ClassModel(name, studyPeriod, semester, year);
-
-            var _UserToken = HttpContext.Session.GetString("_UserToken");
-
-            if (firebase.VerifyLoggedIn(_UserToken).Result == false)
-            {
-                return Forbid();
-            }
-
-            string? success = await firebase.CreateNewClassAsync(_UserToken, classModel);
-
-            if (success != null)
-            {
-                return Ok(success);
-            }
-
-            return BadRequest();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> LoadAllClasses()
-        {
-            var _UserToken = HttpContext.Session.GetString("_UserToken");
-
-            if (firebase.VerifyLoggedIn(_UserToken).Result == false)
-            {
-                return Forbid();
-            }
-
-            List<ClassModel>? success = await firebase.CollectAllClassAsync(_UserToken);
-
-            if (success != null)
-            {
-                return Ok(success);
-            }
-
-            return BadRequest();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> LoadAClass(string className)
-        {
-            var _UserToken = HttpContext.Session.GetString("_UserToken");
-
-            if (firebase.VerifyLoggedIn(_UserToken).Result == false)
-            {
-                return Forbid();
-            }
-
-            ClassModel? success = await firebase.CollectClassAsync(_UserToken, className);
-
-            if (success != null)
-            {
-                return Ok(success);
-            }
-
-            return BadRequest();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> SaveAClassList(string className, string[] studentList)
-        {
-            var _UserToken = HttpContext.Session.GetString("_UserToken");
-
-            if (firebase.VerifyLoggedIn(_UserToken).Result == false)
-            {
-                return Forbid();
-            }
-
-            bool success = await firebase.SaveAClassListAsync(_UserToken, className, studentList);
-
-            if (success)
-            {
-                return Ok(success);
-            }
-
-            return BadRequest();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
