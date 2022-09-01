34ded9e8681f312a91837b8aadde2a615f9a6326
amnot <amnot@DANIEL-PC>
2022-09-01 10:29:23 +0930

    Flexible side drawer

--- UniSA Radiation Therapy Mock Clinic Scheduler/wwwroot/js/site.js
+++ UniSA Radiation Therapy Mock Clinic Scheduler/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var sidebarOpen = false;
+
+function OpenCloseNav() {
+    sidebarOpen = !sidebarOpen;
+
+    if (sidebarOpen) {
+        document.getElementById("nav-drawer").classList.replace('drawer-collapse', 'drawer-full');
+        document.getElementById("body").classList.replace('drawer-collapse', 'drawer-full');
+    }
+    else {
+        document.getElementById("nav-drawer").classList.replace('drawer-full', 'drawer-collapse');
+        document.getElementById("body").classList.replace('drawer-full', 'drawer-collapse');
+    }
+}
+
+function OpenNavHover() {
+    document.getElementById("nav-drawer").classList.replace('drawer-full', 'drawer-collapse');
+    document.getElementById("body").classList.replace('drawer-full', 'drawer-collapse');
+    sidebarOpen = true;
+}
+
+function CloseNavHover() {
+    document.getElementById("nav-drawer").classList.replace('drawer-collapse', 'drawer-full');
+    document.getElementById("body").classList.replace('drawer-collapse', 'drawer-full');
+    sidebarOpen = false;
+}
